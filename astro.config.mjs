75fec87bd5e80fbc023fbf3795f508942d191779
Adam Horodyski <ad.horodyski@gmail.com>
2023-07-18 17:08:26 +0200

    feat: init a PWA setup using @vite-pwa/astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,47 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import pwa from "@vite-pwa/astro";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: "https://example.com",
+  integrations: [
+    mdx(),
+    sitemap(),
+    pwa({
+      mode: "development",
+      devOptions: {
+        enabled: true,
+        navigateFallbackAllowlist: [/^\/404$/],
+      },
+      base: "/",
+      scope: "/",
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.svg"],
+      manifest: {
+        id: "/",
+        name: "Adam's PWA",
+        description: "This is a sample PWA used only for learning purposes",
+        theme_color: "#ffffff",
+        scope: "/",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+      workbox: {
+        navigateFallback: "/404",
+        globPatterns: ["**/*.{css,js,html,svg,png,ico,txt}"],
+      },
+    }),
+  ],
 });
